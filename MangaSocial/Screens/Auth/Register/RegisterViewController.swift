d56770d43d1de93d266e68bcab7ef8684de946be
diem.tran <diem.tran@gmail.com>
2023-07-26 12:41:16 +0700

    Update Diem

--- MangaSocial/Screens/Auth/Register/RegisterViewController.swift
+++ MangaSocial/Screens/Auth/Register/RegisterViewController.swift
@@ -14,11 +14,12 @@ class RegisterViewController: BaseViewController {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
     @IBOutlet weak var loginButtonSignIn: UIButton!
+    @IBOutlet weak var emailCheckLabel: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
-        
+        emailCheckLabel.isHidden = true
         emailTextField.addPadding(.left(15))
         passwordTextField.addPadding(.left(15))
         confirmPasswordTextField.addPadding(.left(15))
@@ -47,18 +48,20 @@ class RegisterViewController: BaseViewController {
     @IBAction func changeConfirmPassword(_ sender: Any) {
         
     }
+    
     private  func checkValidate() {
         let email = emailTextField.text.asStringOrEmpty()
         let password = passwordTextField.text.asStringOrEmpty()
-        let confirmpassword = confirmPasswordTextField.text.asStringOrEmpty()
         
         var isEmail: Bool = false
         var isPassword: Bool = false
         
         if !email.isValidEmail {
             isEmail = false
+            emailCheckLabel.isHidden = true
         } else {
             isEmail = true
+            emailCheckLabel.isHidden = false
         }
         if !password.isValidPassword {
             isPassword = false
@@ -75,22 +78,26 @@ class RegisterViewController: BaseViewController {
     @IBAction func actionSignIn(_ sender: Any) {
         let email = emailTextField.text.asStringOrEmpty()
         let password = passwordTextField.text.asStringOrEmpty()
-        
-        showCustomeIndicator()
-        RegisterAPI.shared.register(email: email, password: password) { [weak self] result in
-            guard let seft = self else { return }
-            seft.hideCustomeIndicator()
-            switch result {
-            case .success(let success):
-//                guard let user = success.account else {
-                    self?.view.makeToast(success.message, position: .top)
-//                    return
-//                }
-//                self?.navigationController?.setRootViewController(viewController: MainTabBarController(),
-//                     return                                                             controllerType: MainTabBarController.self)
-//                // failure
-            case .failure(let failure):
-                self?.view.makeToast(failure.message, position: .top)
+        let confirmpassword = confirmPasswordTextField.text.asStringOrEmpty()
+      
+        if password != confirmpassword {
+            self.view.makeToast("Hai mật khẩu không khớp")
+        } else {
+            showCustomeIndicator()
+            RegisterAPI.shared.register(email: email, password: password) { [weak self] result in
+                guard let seft = self else { return }
+                seft.hideCustomeIndicator()
+                switch result {
+                case .success(let success):
+                    guard let user = success.account else {
+                        self?.view.makeToast(success.message, position: .top)
+                        return
+                    }
+                    seft.navigationController?.popViewController(animated: true)
+                    
+                case .failure(let failure):
+                    self?.view.makeToast(failure.message, position: .top)
+                }
             }
         }
     }
